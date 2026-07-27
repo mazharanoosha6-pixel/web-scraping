4bd156678b1f9771691034787bbe96875f570404
Anoosha Mazhar <mazharanoosha6@gmail.com>
2026-07-28 02:39:34 +0500

    Add book scraper with CSV export

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Web scraper project started!")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://books.toscrape.com"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+books = soup.find_all("article", class_="product_pod")
+
+book_data = []
+
+for book in books:
+    title = book.find("h3").find("a")["title"]
+
+    price = book.find("p", class_="price_color").text
+
+    rating = book.find("p")["class"][1]
+
+    book_data.append({
+        "Title": title,
+        "Price": price,
+        "Rating": rating
+    })
+
+# Convert data into a table
+df = pd.DataFrame(book_data)
+
+# Save as CSV
+df.to_csv("books.csv", index=False)
+
+print("Scraping completed! Data saved to books.csv")
