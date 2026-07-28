05d731ee2f5b1104ce7eada3858063ea6964a14a
Anoosha Mazhar <mazharanoosha6@gmail.com>
2026-07-28 22:48:56 +0500

    Add structured DAAD cost scraping

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
-url = "https://books.toscrape.com"
+url = "https://www2.daad.de/deutschland/studienangebote/international-programmes/en/detail/7801/"
 
 response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-books = soup.find_all("article", class_="product_pod")
+costs = soup.find("div", id="costs")
 
-book_data = []
+labels = costs.find_all("dt")
+values = costs.find_all("dd")
 
-for book in books:
-    title = book.find("h3").find("a")["title"]
-
-    price = book.find("p", class_="price_color").text
-
-    rating = book.find("p")["class"][1]
-
-    book_data.append({
-        "Title": title,
-        "Price": price,
-        "Rating": rating
-    })
-
-# Convert data into a table
-df = pd.DataFrame(book_data)
-
-# Save as CSV
-df.to_csv("books.csv", index=False)
-
-print("Scraping completed! Data saved to books.csv")
+for label, value in zip(labels, values):
+    print("FIELD:", label.get_text(" ", strip=True))
+    print("VALUE:", value.get_text(" ", strip=True))
+    print("-----------------------------")
